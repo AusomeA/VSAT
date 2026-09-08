9ff940a3eeefddb61c242394c6563dd7fa71a3e8
Ausome <ATAnderson@Alumni.Fullsail.Edu>
2026-09-07 19:39:29 -0600

    Android rounded button fix
    -On android, buttons were too rounded, now they should be fixed

--- apps/ground_control/main.cpp
+++ apps/ground_control/main.cpp
@@ -1,9 +1,11 @@
 #include <QQmlApplicationEngine>
 #include <QGuiApplication>
+#include <QQuickStyle>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+    QQuickStyle::setStyle("Basic");
     QQmlApplicationEngine engine;
 
     engine.loadFromModule("GroundControl", "Main");
